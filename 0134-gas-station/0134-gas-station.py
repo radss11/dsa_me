cbc9b874ed6a854f1ce0e2ee503cbc99d8e2abf5
Radha <kamalwarrr@rknec.edu>
2026-07-21 14:46:33 +0530

    Time: 19 ms (94.15%), Space: 17.9 MB (71.62%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        if sum(gas)<sum(cost):
+            return -1
+        start = 0
+        tank = 0
+
+        for i in range(len(gas)):
+            tank += gas[i]-cost[i]
+            if tank<0:
+                start = i+1
+                tank = 0
+        return start
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        
